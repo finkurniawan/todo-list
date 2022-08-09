d1a3d525f3adf14b71523ddf4a8949f8cc0dbb54
alfin kurniawan aryanto <alfinkurniawan9727@gmail.com>
2022-08-09 22:37:21 +0700

    update db

---
+++ src/config/database.js
@@ -0,0 +1,25 @@
+const { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD } = process.env;
+
+module.exports = {
+  development: {
+    username: DB_USER || 'todos',
+    password: DB_PASSWORD || 'todos',
+    database: DB_NAME || 'todos',
+    host: DB_HOST,
+    dialect: 'postgres',
+  },
+  test: {
+    username: DB_USER || 'todos',
+    password: DB_PASSWORD || 'todos',
+    database: DB_NAME || 'todos',
+    host: DB_HOST,
+    dialect: 'postgres',
+  },
+  production: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    dialect: 'postgres',
+  },
+};
